dd672db26b824380813ce648b774a4a5496b359b
gabriel santos Vieira <GabrielsantosV2@hotmail.com>
2023-05-17 19:16:44 -0300

    Inserção das frases e funcionamento do botão 'nova frase'

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,30 +27,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _random = Random();
+  int posicao = 0;
+
+  var _frases = [
+    "Não se exija demais.",
+    "A maior vitória e não desistir.",
+    "Desistir não é uma opção.",
+    "É em meio a dificuldade que se encontra a oportunidade.",
+    "A fé move montanhas."
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Frases do dia"),
-          backgroundColor: Colors.green,
-        ),
-        body: Container(
-          padding: EdgeInsets.all(40),
-          decoration:
-              BoxDecoration(border: Border.all(width: 3, color: Colors.amber)),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset('images/frases_dia.png'),
-                Text('Batatinha quando nasce , se esparrama pelo chão'),
-                ElevatedButton(
-                  onPressed: (() {}),
-                  child: Text('Nova frase'),
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateColor.resolveWith(
-                          (states) => Colors.green)),
-                )
-              ]),
-        ));
+      appBar: AppBar(
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.green,
+      ),
+      body: _buildEstutura(),
+    );
+  }
+
+  Container _buildEstutura() {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(16),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/frases_dia.png',
+              fit: BoxFit.contain,
+            ),
+            Text(
+              _frases[posicao],
+              style: TextStyle(
+                  fontSize: 17,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+              onPressed: (() {
+                setState(() {
+                  _gerarFrase();
+                });
+              }),
+              child: Text('Nova frase'),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateColor.resolveWith((states) => Colors.green)),
+            )
+          ]),
+    );
+  }
+
+  void _gerarFrase() {
+    posicao = new Random().nextInt(_frases.length);
   }
 }
